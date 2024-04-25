8c4eb1aad4f4a30587117838007d0088b7e1af78
Andrew <hydra1337channel@gmail.com>
2024-04-25 20:37:28 +1000

    Reworked Carousel component

--- frontend/src/shared/ui/Carousel/Carousel.tsx
+++ frontend/src/shared/ui/Carousel/Carousel.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useState } from 'react';
 import { Icon, Button } from '@/shared/ui';
 import { combineClasses } from '@/shared/lib';
 import AliceCarousel, { Responsive } from 'react-alice-carousel';
@@ -7,7 +6,10 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 
 interface CarouselProps {
   readonly children: JSX.Element[] | Element[];
-  readonly countVisibleElements?: number;
+  readonly disableButtonsControls?: boolean;
+  readonly autoPlay?: boolean;
+  readonly infinite?: boolean;
+  readonly animationDuration?: number;
   readonly autoWidth?: boolean;
   readonly mouseTracking?: boolean;
   readonly responsive?: Responsive;
@@ -17,68 +19,59 @@ interface CarouselProps {
 
 export const Carousel: React.FC<CarouselProps> = ({
   children,
-  countVisibleElements = 1,
+  disableButtonsControls,
   autoWidth,
+  autoPlay,
+  infinite,
+  animationDuration,
   mouseTracking,
   responsive,
-  disableDotsControls,
+  disableDotsControls = true,
   className,
 }) => {
   const classes = combineClasses(className);
 
-  const [activeIndex, setActiveIndex] = useState<number>(0);
-
-  const minActiveIndexElement = 0;
-  const maxActiveIndexElement = children?.length
-    ? children.length - countVisibleElements
-    : 0;
-
-  const isDisabledSlidePrev = activeIndex <= minActiveIndexElement;
-  const isDisabledSlideNext = activeIndex >= maxActiveIndexElement;
-
-  const getSlidePrev = () => {
-    if (isDisabledSlidePrev) {
-      return;
-    }
-    setActiveIndex(activeIndex - 1);
+  const renderNextButton = ({ isDisabled }: { isDisabled: boolean }) => {
+    return (
+      <Button
+        className="absolute right-2 top-1/2 translate-y-1/2"
+        variant={'accent'}
+        circle
+      >
+        <Icon icon={'right-arrow'} />
+      </Button>
+    );
   };
 
-  const getSlideNext = () => {
-    if (isDisabledSlideNext) {
-      return;
-    }
-    setActiveIndex(activeIndex + 1);
+  const renderPrevButton = ({ isDisabled }: { isDisabled: boolean }) => {
+    return (
+      <Button
+        className="absolute left-2 top-1/2 translate-y-1/2"
+        variant={'accent'}
+        circle
+      >
+        <Icon icon={'left-arrow'} />
+      </Button>
+    );
   };
 
   return (
     <div className={classes}>
       <AliceCarousel
+        autoPlay={autoPlay}
+        disableButtonsControls={disableButtonsControls}
+        infinite={infinite}
         autoWidth={autoWidth}
+        animationDuration={animationDuration}
         mouseTracking={mouseTracking}
         responsive={responsive}
-        activeIndex={activeIndex}
         items={children}
-        disableButtonsControls
         disableDotsControls={disableDotsControls}
+        //@ts-ignore
+        renderPrevButton={renderPrevButton}
+        //@ts-ignore
+        renderNextButton={renderNextButton}
       />
-      <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-        <Button
-          variant={'accent'}
-          circle
-          onClick={getSlidePrev}
-          disabled={isDisabledSlidePrev}
-        >
-          <Icon icon={'left-arrow'} />
-        </Button>
-        <Button
-          variant={'accent'}
-          circle
-          onClick={getSlideNext}
-          disabled={isDisabledSlideNext}
-        >
-          <Icon icon={'right-arrow'} />
-        </Button>
-      </div>
     </div>
   );
 };
